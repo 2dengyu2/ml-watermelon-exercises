e453876a20a254fc290a079e5ec34865114a96e3
2dengyu2 <649355204@qq.com>
2022-11-12 17:36:20 +0800

    feat: 基本框架、数据结构设计及思路确定

---
+++ ex5.5-bp/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+# @Time    : 2022/11/12 16:28
+# @Author  : liangliang
+# @Email   : 649355204@qq.com
+# @File    : __init__.py
+# @Software: PyCharm
---
+++ ex5.5-bp/standard_bp.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+# 标准BP算法
+# @Time    : 2022/11/12 16:14
+# @Author  : liangliang
+# @Email   : 649355204@qq.com
+# @File    : standard_bp.py
+# @Software: PyCharm
+from dataset.InitDataset import init_dataset
+
+learning_rate = 0.1
+
+
+def training():
+    return None
+
+
+def save(nn):
+    return None
+
+
+if __name__ == '__main__':
+    D = init_dataset('3.0')
+    n = learning_rate
+    MFNN = training()
+    save(MFNN)
